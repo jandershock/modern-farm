b9774c46f046bb2e4e20e7ac833ac1bfdaca8305
jandershock <jordan.andershock@gmail.com>
2022-02-18 16:06:11 -0600

    Added all seed functions Placed seed exports into one seed-index.js file for easier importing.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,14 @@
 import * as plan from './plan.js';
+import * as seeds from './seeds/seeds-index.js';
 
 console.log("Welcome to the main module")
 
 let yearlyPlan = plan.createPlan();
 
 console.log(yearlyPlan);
+console.log(seeds.createAsparagus());
+console.log(seeds.createCorn());
+console.log(seeds.createPotato());
+console.log(seeds.createSoybean());
+console.log(seeds.createSunflower());
+console.log(seeds.createWheat());
---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,7 @@
+export const createCorn = () => {
+    return Array(2).fill({
+        type: 'Corn',
+        height: 180,
+        output: 6
+    })
+}
