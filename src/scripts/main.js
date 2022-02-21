9b2525dad6567b84088b626fc655be962ce6dd22
jandershock <jordan.andershock@gmail.com>
2022-02-21 09:17:48 -0600

    Created field functions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,22 @@
 import * as plan from './plan.js';
 import * as seeds from './seeds/seeds-index.js';
+import * as field from './field.js';
 
 console.log("Welcome to the main module")
 
-let yearlyPlan = plan.createPlan();
+// let yearlyPlan = plan.createPlan();
 
-console.log(yearlyPlan);
-console.log(seeds.createAsparagus());
-console.log(seeds.createCorn());
-console.log(seeds.createPotato());
-console.log(seeds.createSoybean());
-console.log(seeds.createSunflower());
-console.log(seeds.createWheat());
+// console.log(yearlyPlan);
+// console.log(seeds.createAsparagus());
+// console.log(seeds.createCorn());
+// console.log(seeds.createPotato());
+// console.log(seeds.createSoybean());
+// console.log(seeds.createSunflower());
+// console.log(seeds.createWheat());
+
+
+let seedObject = seeds.createAsparagus();
+field.addPlant(seedObject);
+field.addPlant(seeds.createCorn());
+let fieldArray = field.usePlants();
+console.log(fieldArray);
